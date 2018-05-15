7b5d706f9a918572b095adb823489571ec5d3595
Hangil Chung <hangil@Hangils-MacBook-Pro.local>
2018-05-15 03:13:33 -0400

    included writeup

--- run.py
+++ run.py
@@ -175,14 +175,118 @@ def get_btwness_seed(G, inner, outer, budgeted, p):
         seed_pairs = heapq.nlargest(num_seeds, heap)
         seeds = [x[1] for x in seed_pairs]
         return seeds
+
+
+def _get_A_k(A_1, A_prev_k):
+    num_inner = len(A_1)
+    num_total = len(A_1[0])
+    A_k = [[[] for _ in range(num_total)] for _ in range(num_inner)]
+
+    for i in range(num_inner):
+        for j in range(num_total):
+
+            if i == j:
+                A_k[i][j] = []
+                continue
+
+            if A_prev_k[i][j] != [] and j < num_inner:
+                for l in range(num_total):
+                    if A_1[j][l] == 1 and i != l and l not in A_prev_k[i][j]:
+                        A_k[i][l] = A_prev_k[i][j] + [l]# path from i to j
+                    
+    return A_k
+            
+
+def get_katz_seed(G, inner, outer, budgeted, p):
+    num_inner = len(inner)
+    num_total = len(inner) + len(outer)
+    iterations = 5
+    A_mats = [None for _ in range(iterations)]    # Store A_k's
+    inner_node_idx_to_id = {node_id: idx for idx, node_id in enumerate(inner)}
+    for idx, node_id in enumerate(outer):
+        inner_node_idx_to_id[node_id] = idx + len(inner)
+    
+    # Construct adjacent matrix
+    adj_mat = np.zeros((num_inner, num_total))
+    for node_idx, inner_node in enumerate(inner):
+        for neighbor in G.edges[inner_node]:
+            adj_mat[node_idx][inner_node_idx_to_id[neighbor]] = 1
+            
+    A_mats[0] = [[[j] if adj_mat[i, j] > 0 else [] for j in range(num_total) ]
+                 for i in range(num_inner)]
+
+    for i in range(1, iterations):
+        A_mats[i] = _get_A_k(A_mats[0], A_mats[i - 1])
+        
+    A_binary = []
+    for A_i in A_mats:
+        cur_A = np.zeros((num_inner, num_total))
+        for i in range(num_inner):
+            for j in range(num_total):
+                if A_i[i][j] == None:
+                    pass
+                else:
+                    if len(A_i[i][j]) > 0:
+                        if j < num_inner:
+                            cur_A[i, j] = 1
+                        else:
+                            cur_A[i,j] = -1
+        A_binary.append(cur_A)
+
+    centrality = np.zeros(len(inner))
+    for idx, node in enumerate(inner):
+        for k in range(iterations):
+            try:
+                centrality[idx] += np.power(p, k + 1) * np.sum(A_binary[k][idx])
+            except Exception as e:
+                print "Let Soobin know that he screwed up."
+                raise e
+
+    nodes_by_centrality = np.argsort(-centrality)
+
+    if budgeted:
+        num_seeds = 1
+    else: 
+        num_seeds = int((1-p) * len(inner))
+
+    return [inner[nodes_by_centrality[i]] for i in range(num_seeds)]
+
+def get_katz_seed_nx(G, inner, outer, budgeted, p):
+    # Set up NX graph
+    nx_graph = nx.Graph()
+    nx_graph.add_nodes_from(G.nodeThetas.keys())
+    edges_added = set()
+    for pair, prob in G.edgeProb.items():
+        if (pair[0], pair[1]) in edges_added or pair[0] in outer or pair[1] in outer:
+            continue
+        nx_graph.add_edge(pair[0], pair[1], weight = 1.0/prob)
+        edges_added.add((pair[0], pair[1]))
+        edges_added.add((pair[1], pair[0]))
+
+    katz_by_node = nx.katz_centrality_numpy(nx_graph, alpha = p)
+    max_cent = 0
+    seed = None
+    for node_id, cur_cent in katz_by_node.items():
+        if cur_cent > max_cent:
+            max_cent = cur_cent
+            seed = node_id
+    
+    nodes_sorted_by_katz = sorted(katz_by_node, key=katz_by_node.get, reverse=True)
+
+    if budgeted:
+        num_seeds = 1
+    else: 
+        num_seeds = int((1-p) * len(inner))
+
+    return [inner[nodes_sorted_by_katz[i]] for i in range(num_seeds)]
             
 
 
 def run():
 
     random.seed(0)
-
-    header = ['Theta', 'Ratio', 'Prob', 'HD', 'Rand', 'Far', 'Near', "Btw"]
+    header = ['Theta', 'Inner', 'Outer', 'Ratio', 'Prob', 'Opt',
+              'HD', 'Rand', 'Far', 'Near', "Btw", 'katz']
     results = []
     results_budgeted = []
 
@@ -204,12 +308,10 @@ def run():
             pickle.dump(SG, open(graph_file, 'wb'))
 
         end = time.time()
-        print "Calculating Graph: {0}".format((start - end)/60)
 
         start = time.time()
         SG.calculate_btwness(theta)
         end = time.time()
-        print "Calculating btwness: {0}".format((start - end)/60)
 
         inner, outer = SG.get_inner_outer_nodes(theta)
 
@@ -228,11 +330,9 @@ def run():
             start = time.time()
             SG.set_edge_probabilities(prob_func)
             SG.calculate_distance_matrix_with_prob(p)
-            end = time.time()
-            print "Calculating distMP: {0}".format((start - end)/60)
 
             algs = [get_highest_deg_seed, get_random_seed, get_furthest_seed,
-                    get_nearest_seed, get_btwness_seed]
+                    get_nearest_seed, get_btwness_seed, get_katz_seed]
             
             scores = [0.0] * len(algs)
             scores_budgeted = [0.0] * len(algs)
@@ -252,7 +352,7 @@ def run():
                     scores_budgeted[ind] += G_theta.get_seed_score(seed)
                     
             end = time.time()
-            print "Calculating Seed: {0}".format((start - end)/60)
+            print "Calculating Seed: {0}".format((end - start)/60)
 
             norm_scores = [(x / NUM  + len(outer)) / best_possible for x in scores]
             norm_scores_budgeted = [(x / NUM  + len(outer)) / best_possible
